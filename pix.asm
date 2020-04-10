8d04fc6f208da449338e06ba14bde89d172b7dfc
Karol Soczewica <ks394468@students.mimuw.edu.pl>
2020-04-10 14:41:03 +0200

    dodane komentarze, ale bez stylu

--- pix.asm
+++ pix.asm
@@ -24,7 +24,7 @@ extern pixtime
 ; Oblicza część ułamkową liczby %1 / %2.
 ; Modyfikuje rejestry rax, rdx, rcx.
 ; Wynik przekazuje w rejestrze rax.
-%macro div_frac 2
+%macro DIV_FRAC 2
   xor eax, eax
   mov rdx, %1
   mov rcx, %2
@@ -33,10 +33,10 @@ extern pixtime
 
 ; Oblicza (16 ^ %1) mod %2 i przekazuje wynik w r8.
 ; Modyfikuje rejestry rax, rdx, rcx, r8 i rejestr pod %1
-%macro pow_mod 2
+%macro POW_MOD 2
   test %1, %1
   jz %%check_mod ; Wykładnik jest równy 0.
-  mov rax, 0x10
+  mov rax, 16
   xor edx, edx
   div %2
   test rdx, rdx ; Sprawdź, czy mod dzieli 16.
@@ -74,7 +74,7 @@ extern pixtime
 ; Modyfikuje rejestry rbx, r10 i rejestry modyfikowane przez procedurę s_j.
 ; Wynik zwraca w rejestrze r10.
 ; Dokładniej, oblicza 4S_1 - 2S_4 - S_5 - S_6, ale w taki sposób: (2S_1 - S_4) + (2S_1 - S_4) - S_5 - S_6.
-%macro pi 0
+%macro PI 0
   xor r10d, r10d
   mov rbx, 1 ; W rbx przekazujemy wartość j do procedury s_j.
   call s_j
@@ -92,14 +92,14 @@ extern pixtime
 %endmacro
 
 ; Wrzuca na stos rejestry z parametrami i woła funkcję pixtime.
-%macro time 0
+%macro TIME 0
   push rdi
   push rsi
   push rdx
-  rdtsc
+  rdtsc ; Wynik w edx:eax.
   shl rdx, 32
   add rdx, rax
-  mov rdi, rdx
+  mov rdi, rdx ; Łączny wynik zapisujemy na rdi, czyli na pierwszym parametrze.
   call pixtime
   pop rdx
   pop rsi
@@ -107,66 +107,71 @@ extern pixtime
 %endmacro
 
 section .text
-align 8
+; Oblicza S_j i przekazuje wynik w r9.
+; Modyfikuje rejestry rax, rdi, rbp, rcx, r9,
+; r14 i inne rejestry modyfikowane z makr DIV_FRAC i POW_MOD.
+; Dostaje jako argumenty: liczbę n w rejestrze r15 i liczbę j w rejestrze rbx.
 s_j:
-  xor r9d, r9d ; <--------- wynik
-  xor r14d, r14d ; k
+  xor r9d, r9d
+  xor r14d, r14d ; Pod r14 mamy k.
 .first_loop:
   cmp r14, r15
-  ja .first_loop_exit
-  mov rdi, r14 ; rdi = mianownik
+  ja .first_loop_exit ; k >= n
+  mov rdi, r14 ; W rdi mamy mianownik.
   shl rdi, 3
-  add rdi, rbx
+  add rdi, rbx ; rdi = 8 * k + j
   mov rbp, r15
-  sub rbp, r14 ; n-k
-  pow_mod rbp, rdi
-  div_frac r8, rdi
-  add r9, rax ; rax??????
-  inc r14 ; zwieksz k
+  sub rbp, r14 ; rbp = n - k
+  POW_MOD rbp, rdi
+  DIV_FRAC r8, rdi ; W r8 jest wynik POW_MOD.
+  add r9, rax ; W rax jest wynik DIV_FRAC.
+  inc r14
   jmp .first_loop
 .first_loop_exit:
-  mov rbp, 0x1000000000000000
-  mov rcx, rbp ; rcx = licznik
+  mov rbp, 0x1000000000000000 ; Część ułamkowa liczby 1/16.
+  mov rcx, rbp ; W rcx mamy licznik.
 .second_loop:
   mov rdi, r14
   shl rdi, 3
-  add rdi, rbx
+  add rdi, rbx ; rdi = 8 * k + j
   xor rdx, rdx
   mov rax, rcx
   div rdi
   test rax, rax
-  jz .exit
-  add r9, rax ; rdx????
+  jz .exit ; rcx / rdi = 0
+  add r9, rax ; Dodajemy do wyniku rcx / rdi
   xor rdx, rdx
   mov rax, rcx
   mul rbp
-  mov rcx, rdx
+  mov rcx, rdx ; W rcx mamy część ułamkową liczby (rcx / rdi) * (1 / 16).
   inc r14
   jmp .second_loop
 .exit:
   ret
+align 8
 pix:
-  time
+  TIME ; Wywołaj funkcję pixtime.
   PUSH_REGS
-  lea r11, [rdi]
-  mov r13, rdx
-  push rdi
+  lea r11, [rdi] ; Adres tablicy, do której zapisujemy wyniki.
+  mov r13, rdx ; Wartość parametru max.
+  push rdi ; Wrzucam na stos rdi i rdx na wszelki wypadek.
   push rdx
 pix_loop:
-  mov r12, 1
-  lock xadd [rsi], r12
+  mov r12, 1 ; Wartość, którą dodajemy do indeksu elementu w tablicy ppi.
+  lock \
+  xadd [rsi], r12 ; Atomowe pobranie wartości z pidx do r12 i dodanie 1 do pidx.
   cmp r12, r13
-  jae exit
+  jae exit ; Indeks jest większy lub równy max, więc kończymy.
   mov r15, r12
-  shl r15, 3
-  pi
+  shl r15, 3 ; r15 = 8 * m
+  PI
   shr r10, 32
-  mov dword [r11 + 4 * r12], r10d
+  mov dword [r11 + 4 * r12], r10d ; Zapisujemy wynik do odpowiedniego miejsca w tablicy.
   jmp pix_loop
 exit:
   pop rdx
   pop rdi
   POP_REGS
-  xor eax, eax
-  time
+  xor eax, eax ; Zwróć NULL.
+  TIME ; Wywołaj funkcję pixtime.
   ret
