f2cab8a257c1aab477de9a32d9ba153965a2591b
Karol Soczewica <37833408+soczil@users.noreply.github.com>
2020-04-20 14:51:43 +0200

    Update pix.c

--- pix.c
+++ pix.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 #include "pix.h"
 
+void pixtime(uint64_t clock_tick) {
+  fprintf(stderr, "%016lX\n", clock_tick);
+}
+
 int main() {
     const int N = 8;
     uint32_t table[N];
     uint64_t index = 0;
-    uint64_t jol = pix(table, &index, N);
-
-    printf("%lu\n", jol);
+    pix(table, &index, N);
     
     for (int i = 0; i < N; i++) {
         printf("%u\n", table[i]);
